7d7786d0e6ae8099ecf135f540ef651a134cc1f4
meeplabs123 <76091175+meeplabs123@users.noreply.github.com>
2022-05-12 07:07:50 +0100

    Update start.bash

--- start.bash
+++ start.bash
@@ -1,3 +1,6 @@
+bash refresh.sh
+
+bash init.sh
 bash authtoken.sh
 clear
 ./ngrok tcp 25565 &
@@ -5,9 +8,11 @@ sleep 5
 SRVIP=$(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url | awk '{gsub("tcp://",""); print}')
 clear
 reset
-python website.py "$SRVIP" &
+python3 website.py "$SRVIP" &
 echo "Server Ip:"
 echo "$SRVIP"
 echo ""
 java -jar paper.jar
 clear
+
+bash refresh.sh
